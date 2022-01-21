a66b7edfd6f2313ead97f0d9c745d8c09d6ba7e9
koussailakadi <kadi.koussaila@outlook.fr>
2022-01-21 20:58:23 +0100

    first commit

--- mrcnn/m_rcnn.py
+++ mrcnn/m_rcnn.py
@@ -14,7 +14,7 @@ import shutil
 import zipfile
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("/content/Mask_RCNN")
+ROOT_DIR = os.path.abspath("./")
 print("VERS 0.2")
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
@@ -37,6 +37,51 @@ COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 # Download COCO trained weights from Releases if needed
 if not os.path.exists(COCO_MODEL_PATH):
     utils.download_trained_weights(COCO_MODEL_PATH)
+
+
+class CustomConfig(Config):
+    def __init__(self, num_classes):
+
+        if num_classes > 1:
+            raise ValueError("{} classes were found. This is a DEMO version, and it only supports 1 class. Get the PRO version to"
+                  " continue the training.".format(num_classes))
+
+        classes_number = num_classes
+        super().__init__()
+    """Configuration for training on the toy shapes dataset.
+    Derives from the base Config class and overrides values specific
+    to the toy shapes dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "object"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 4
+
+    # Number of classes
+    NUM_CLASSES = 1 + 1
+
+    # Use small images for faster training. Set the limits of the small side
+    # the large side, and that determines the image shape.
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+
+    # Use smaller anchors because our image and objects are small
+    # RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    # TRAIN_ROIS_PER_IMAGE = 32
+
+    # Use a small epoch since the data is simple
+    STEPS_PER_EPOCH = 500
+
+    # use small validation steps since the epoch is small
+    VALIDATION_STEPS = 5
+
+    DETECTION_MIN_CONFIDENCE = 0.9
 
 
 
@@ -98,13 +143,16 @@ class CustomDataset(utils.Dataset):
         # Get all images and add them to the dataset
         seen_images = {}
 
-        # Split the dataset, if train, get 90%, else 10%
+        # Split the dataset, train 80% , val 10%, test 10%:  
         len_images = len(coco_json['images'])
         if dataset_type == "train":
-            img_range = [int(len_images / 9), len_images]
-        else:
-            img_range = [0, int(len_images / 9)]
+            img_range = [int(len_images / 8), len_images]
+        elif dataset_type == "val":
+            img_range = [int(len_images / 16), int(len_images / 8)]
+        else: # test:
+            img_range = [0, int(len_images / 16)]
 
+        
         for i in range(img_range[0], img_range[1]):
             image = coco_json['images'][i]
             image_id = image['id']
@@ -131,6 +179,7 @@ class CustomDataset(utils.Dataset):
                     height=image_height,
                     annotations=image_annotations
                 )
+             
 
     def load_mask(self, image_id):
         """ Load instance masks for the given image.
